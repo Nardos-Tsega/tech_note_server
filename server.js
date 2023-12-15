900b2de92ccc4b99b82edcbfe0a22d6a9d4e6ab8
Nardos <nardos@Mintes-MacBook-Pro.local>
2023-12-15 16:18:01 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+
+console.log(process.env.NODE_ENV);
+
+connectDB();
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(logger);
+app.use(cookieParser());
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", require("./routes/root"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/notes", require("./routes/noteRoutes"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "Page not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
+
+app.use(errorHandler);
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+
+  app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log(error);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
